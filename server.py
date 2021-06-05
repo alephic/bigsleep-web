e4f415b9c575a25bff8017d056977c32afa716b6
Kaj Bostrom <bostromk@cs.washington.edu>
2021-06-04 23:39:41 -0500

    wip

--- server.py
+++ server.py
@@ -4,7 +4,10 @@ import asyncio
 import signal
 import subprocess
 
+loop = asyncio.get_event_loop()
+
 bigsleep_process = None
+active_prompt = ""
 
 def get_filename_from_prompt(prompt):
     return prompt.replace("-", "_").replace(",", "").replace(" ", "_").replace("|", "--").strip('-_')[:255] + '.png'
@@ -13,13 +16,27 @@ async def serve_interface(req):
     return web.FileResponse('interface.html')
 
 async def handle_prompt(req):
-    global bigsleep_process
+    global bigsleep_process, active_prompt
     if bigsleep_process is not None:
         bigsleep_process.send_signal(signal.SIGINT)
         bigsleep_process.wait()
     params = req.rel_url.query
     bigsleep_process = subprocess.Popen(["dream", params['prompt']])
+    active_prompt = params['prompt']
 
 async def handle_poll(req):
-    params = req.rel_url.query
-    return web.FileResponse(get_filename_from_prompt(params['prompt']))
+    global active_prompt
+    return web.FileResponse(get_filename_from_prompt(active_prompt))
+
+async def report_active_prompt(req):
+    global active_prompt
+    return web.Response(text=active_prompt)
+
+async def start_server():
+    server = web.Application(loop=loop)
+    server.add_routes([
+        web.get('/image', handle_poll),
+        web.get('/activeprompt', report_active_prompt),
+        web.get('/', serve_interface),
+        web.get('/update', handle_prompt)
+    ])
